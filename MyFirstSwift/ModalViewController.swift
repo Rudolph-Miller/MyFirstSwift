cbe24319208a356344ae2deceaea0306f70ae33d
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-10-18 23:25:58 +0900

    Add modal view

---
+++ MyFirstSwift/ModalViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ModalViewController.swift
+//  MyFirstSwift
+//
+//  Created by Rudolph Miller on 2015/10/18.
+//  Copyright © 2015年 Rudolph Miller. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonHandler(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true , completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
